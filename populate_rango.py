75958f7f26c4993b75e5248d8715460c4ca16be1
My name <dzisvolodymyr@gmail.com>
2016-11-21 22:56:11 +0100

    end of part 5'models and databases'

--- populate_rango.py
+++ populate_rango.py
@@ -40,16 +40,23 @@ def populate():
       "url":"http://flask.pocoo.org"}]
 
 
-    cats = {"Python": {"pages": python_pages},
-             "Django": {"pages": django_pages},
+    cats = {"Python": {"pages": python_pages,
+                       "views":128,
+                       "likes":32},
+             "Django": {"pages": django_pages,
+                        "views":64,
+                        "likes":32
+                        },
 
-    "Other Frameworks": {"pages": other_pages}}
+    "Other Frameworks": {"pages": other_pages,
+                         "views":32,
+                        "likes":16}
+            }
 
     for cat, cat_data in cats.items():
-        c = add_cat(cat)
+        c = add_cat(cat, cat_data["views"], cat_data["likes"])
         for p in cat_data["pages"]:
             add_page(c, p["title"],p["url"])
-
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
             print("- {0} - {1}".format(str(c), str(p)))
@@ -61,8 +68,10 @@ def add_page(cat,title,url,views=0):
     p.save()
     return p
 
-def add_cat(name):
+def add_cat(name, views=0,likes=0):
     c = Category.objects.get_or_create(name=name)[0]
+    c.views = views
+    c.likes = likes
     c.save()
     return c
 if __name__=='__main__':
--- rango/models.py
+++ rango/models.py
@@ -2,8 +2,16 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
     def __str__(self): # For Python 2, use __unicode__ too
      return self.name
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+        def __str__(self):
+            return self.name
+
 class Page(models.Model):
     category = models.ForeignKey(Category)
     title = models.CharField(max_length=128)
