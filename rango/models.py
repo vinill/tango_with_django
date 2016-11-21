7fc4c7483d2ff6e47b92723770b28241b36d13b0
My name <dzisvolodymyr@gmail.com>
2016-11-21 15:52:58 +0100

    create models and setting up see page 48

--- rango/models.py
+++ rango/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    def __str__(self): # For Python 2, use __unicode__ too
+     return self.name
+class Page(models.Model):
+    category = models.ForeignKey(Category)
+    title = models.CharField(max_length=128)
+    url = models.URLField()
+    views = models.IntegerField(default=0)
+    def __str__(self): # For Python 2, use __unicode__ too
+        return self.title
+
+
+
+
+#TODO TEST FILE FIELD https://docs.djangoproject.com/es/1.9/ref/models/fields/#model-field-types
