ed24ad2f38a6292854fc8aec2cdaec0d4389a4ba
My name <dzisvolodymyr@gmail.com>
2016-11-21 01:02:29 +0100

    end the Templates and Media files part, create media and static files directory

--- rango/views.py
+++ rango/views.py
@@ -1,3 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    context_dict={'boldmessage': "boldmessage!!"}
+    return render(request, 'rango/index.html', context=context_dict)
+def about(request):
+    return HttpResponse("rango says hello in about page!")
