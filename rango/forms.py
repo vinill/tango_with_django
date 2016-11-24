12b1456900c350ba2ce2caba6a652a1b981f2c92
My name <dzisvolodymyr@gmail.com>
2016-11-25 00:05:52 +0100

    finish part 7

--- rango/forms.py
+++ rango/forms.py
@@ -18,16 +18,16 @@ class PageForm(forms.ModelForm):
     url = forms.URLField(max_length=200,help_text = "Please enter the URL of the page.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
 
-    def clean(self):
-        cleaned_data = self.cleaned_data
-        url = cleaned_data.get('url')
-
-
-        if url and not url.startwith('http://'):
-            url = 'http://' + url
-            cleaned_data['url'] = url
-
-            return cleaned_data
+    # def clean(self):
+    #     cleaned_data = self.cleaned_data
+    #     url = cleaned_data.get('url')
+    #
+    #
+    #     if url and not url.startwith('http://'):
+    #         url = 'http://' + url
+    #         cleaned_data['url'] = url
+    #
+    #         return cleaned_data
 
 
 
