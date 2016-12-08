08da1f5c5a6b2ce2d09816ce4068076b9890da96
My name <dzisvolodymyr@gmail.com>
2016-12-09 00:52:54 +0100

    register working

--- rango/admin.py
+++ rango/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
+
 from rango.models import Category, Page
+from rango.models import UserProfile
 
 admin.site.register(Category)
 admin.site.register(Page)
+admin.site.register(UserProfile)
--- rango/templatetags/rango_template_tags.py
+++ rango/templatetags/rango_template_tags.py
@@ -4,7 +4,7 @@ from rango.models import Category
 register = template.Library()
 
 
-@register.inclusion_tag('rango/cats.html')
+@register.inclusion_tag("rango/cats.html")
 def get_category_list(cat=None):
     return {'cats': Category.objects.all(),
             'act_cat': cat}
